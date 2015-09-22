4ef9342ee777126c105c4cfc099a3329f75b6926
Maciej Kucharski <maciek.zir@gmail.com>
2015-09-22 16:15:23 +0200

    [master] action and event names

---
+++ tweetworld/app/assets/lib/constants.js
@@ -0,0 +1,15 @@
+module.exports = {
+    EVENTS: {
+        CHANGE: "change"
+    },
+    SOCKET_EVENTS: {
+        NEW_TWEET: "tweet:new",
+        CONNECTED: "connection",
+        DISCONNECTED: "disconnect"
+    },
+    ACTIONS: {
+        ADD_TWEET: "tweet:add",
+        REMOVE_TWEET: "tweet:remove",
+        CLEAR_TWEETS: "tweet:clear"
+    }
+};
