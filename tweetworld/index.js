9b0135c85250880dc1bb474ad1cb26bbbfce539c
Maciej Kucharski <maciek.zir@gmail.com>
2015-09-22 16:18:51 +0200

    [master] send tweets from stream to websockets

--- tweetworld/index.js
+++ tweetworld/index.js
@@ -1,9 +1,30 @@
+"use strict";
 var config = require("./config.js");
-var io = require("socket.io");
+var Server = require("socket.io");
 var TwitterStream = require('./modules/tweet_stream');
 
 var stream = TwitterStream.new(config.twitter);
 stream.start();
-stream.on('tweet:new', function(tweet) {
-   console.log(tweet);
+
+const io = new Server(config.server.port);
+const  users = new Set();
+io.on('connection', function(socket) {
+   if(users.size === 0) {
+      stream.on('tweet:new', function(tweet) {
+         if(tweet.id && tweet.place) {
+            socket.broadcast.emit("tweet:new", tweet);
+            socket.emit("tweet:new", tweet);
+         }
+      });
+   }
+   users.add(socket.id);
+
+   socket.on('disconnect', function() {
+
+      users.delete(socket.id);
+      if (users.size === 0) {
+         stream.removeAllListeners("tweet:new");
+      }
+   });
 });
+
